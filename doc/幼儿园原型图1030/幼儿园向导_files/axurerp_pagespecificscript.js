35967e9975d46bf86532930daa4b4209d4059370
hopesfish <hopesfish@163.com>
2013-11-05 17:36:44 +0800

    提交原型

---
+++ doc/幼儿园原型图1030/幼儿园向导_files/axurerp_pagespecificscript.js
@@ -0,0 +1,7 @@
+﻿for(var i = 0; i < 21; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+
+$axure.eventManager.pageLoad(
+function (e) {
+
+});
+gv_vAlignTable['u20'] = 'top';gv_vAlignTable['u19'] = 'top';gv_vAlignTable['u18'] = 'center';gv_vAlignTable['u16'] = 'center';gv_vAlignTable['u10'] = 'center';gv_vAlignTable['u12'] = 'center';gv_vAlignTable['u14'] = 'center';gv_vAlignTable['u1'] = 'center';gv_vAlignTable['u3'] = 'center';gv_vAlignTable['u4'] = 'top';gv_vAlignTable['u6'] = 'center';gv_vAlignTable['u8'] = 'center';
