c35250b7ab561f502cf5983f6c889125364c1209
hopesfish <hopesfish@163.com>
2013-11-28 13:20:00 +0800

    update music

--- web/wxbot/gateway/music.js
+++ web/wxbot/gateway/music.js
@@ -9,10 +9,9 @@ var conf = require('../../conf');
 
 function child_music(info, next) {
     var text = ejs.render(
-        '使用方法:\n\nQQ音乐电台\n1，<a href="<%- ur1%>">点击这里立刻收听</a>\n\n百度随心听：\n1，<a href="<%- ur2%>">点击这里</a>\n2，向右滑动界面\n3，选择【儿歌】频道', 
+        '百度随心听使用方法：\n1，<a href="<%- url%>">点击这里</a>\n2，向右滑动界面\n3，选择【儿歌】频道', 
         {
-        	url1: 'http://fm.qq.com/#166_1',
-            url2: 'http://fm.baidu.com/#/channel/public_tag_%E5%84%BF%E6%AD%8C'
+            url: 'http://fm.baidu.com/#/channel/public_tag_%E5%84%BF%E6%AD%8C'
         }
     );
     return next(null, text);
