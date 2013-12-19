0183b3a4106894724bbf0a41d0ec8915ff9e4d4e
hopesfish <hopesfish@163.com>
2013-12-19 14:55:39 +0800

    add test case for parent add

--- test/test.js
+++ test/test.js
@@ -18,4 +18,44 @@ forever start -o out.log -e err.log app.js
 weixiaohuodong
 localhost:3000/api/school/d28eefe9-db3b-4db5-a469-424ac5d187d8/syncmenu?appId=wx7e623947327e472e&appSecret=2964baf9c94a1417a54719294f241e0a
 */
-require("./wxbot")();
+//require("./wxbot")();
+
+var request = require('request');
+var crypto = require('crypto');
+var shasum = crypto.createHash('md5');
+
+var key = '1387432923256';
+shasum.update(key + 'rest' + 'kinderg' + '1qw23er4' + '20131219');
+var token = shasum.digest('hex');
+
+var mobile = "138" + (new Date()).getTime().toString().substring(5);
+
+var options = {
+    url: 'http://test.weexiao.com/api/parent',
+    method: 'POST',
+    headers: {
+        'wexkey': key,
+        'wextoken': token,
+    },
+    form: {
+    	schoolId: "d28eefe9-db3b-4db5-a469-424ac5d187d8",
+    	studentId: "2b275c44-50fa-11e3-83f9-00163e0426cb",
+    	name: "test" + mobile,
+    	mobile: mobile,
+    	photo: "photo"+mobile,
+    }
+};
+
+console.info(options);
+
+function callback(error, response, body) {
+    if (!error && response.statusCode == 200) {
+    	console.info("done");
+        console.log(body);
+    } else {
+    	console.info("failed");
+    	console.log(body);
+    }
+}
+
+request(options, callback);
--- test/wxbot/parent_register.js
+++ test/wxbot/parent_register.js
@@ -1,6 +1,36 @@
-var assert = require("assert")
+var assert = require("assert");
+var request = require('request');
+
 module.exports = function() {
 	describe('test parent register', function(){
+	});
+	describe('test parent add by parent', function(done){
+
+		var options = {
+		    url: 'http://test.weexiao.com/webot/api/parent',
+		    headers: {
+		        'wexkey': 'request',
+		        'wextoken': 'request'
+		    }
+		};
+
+		function callback(error, response, body) {
+		    if (!error && response.statusCode == 200) {
+		        var info = JSON.parse(body);
+		        console.log(info.stargazers_count + " Stars");
+		        console.log(info.forks_count + " Forks");
+		    } else {
+		    	console.info("failed..");
+		    }
+		}
+
+		request.post('http://test.weexiao.com/webot/api/parent', {form:{mobile: '13811749918'}}, function (error, response, body) {
+			console.info)
+            if (error) {
+            	throw error;
+            }
+            done();
+        })
 	});
 }
 // webot send --des http://localhost:3000/weixin/api --token weexiao t 认证13811111111
