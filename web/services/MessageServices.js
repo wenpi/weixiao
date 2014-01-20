a1284d67cc784a46aefd4d2b78b5bb1d50403d46
panwei <hopesfish@163.com>
2014-01-20 23:41:23 +0800

    print err

--- web/services/MessageServices.js
+++ web/services/MessageServices.js
@@ -14,66 +14,67 @@ var BaseServices = require("./BaseServices");
  */
 module.exports.create = function(schoolId, user, message) {
     var deferred = Q.defer(),
-    	url = conf.site_root + '/api/school/' + schoolId + '/teacher/' + user.teacherId + '/message',
-    	data = {
-    		'Message[title]': message.title || '',
-    		'Message[content]:': message.content || '',
-    		'Message[top]:': message.top || '0',
-    		'userid': user.id,
-    		'sendsms': message.sms || '0'
-    	};
+        url = conf.site_root + '/api/school/' + schoolId + '/teacher/' + user.teacherId + '/message',
+        data = {
+            'Message[title]': message.title || '',
+            'Message[content]:': message.content || '',
+            'Message[top]:': message.top || '0',
+            'userid': user.id,
+            'sendsms': message.sms || '0'
+        };
 
     if (message.studentId) {
-    	url = conf.site_root + '/api/school/' + schoolId + '/student/' + message.studentId + '/message';
+        url = conf.site_root + '/api/school/' + schoolId + '/student/' + message.studentId + '/message';
     }
 
-	var options = {
-	    url: url,
-	    method: 'POST',
-	    headers: BaseServices.getAuthoriedHeader(),
-	    form: data
-	};
+    var options = {
+        url: url,
+        method: 'POST',
+        headers: BaseServices.getAuthoriedHeader(),
+        form: data
+    };
 
-	function callback(error, response, body) {
-	    if (!error && response.statusCode == 201) {
-	        deferred.resolve();
-	    } else {
-	    	deferred.reject();
-	    }
-	}
+    function callback(error, response, body) {
+        if (!error && response.statusCode == 201) {
+            deferred.resolve();
+        } else {
+            console.info(response.body);
+            deferred.reject();
+        }
+    }
 
-	request(options, callback);
+    request(options, callback);
 
-	return deferred.promise;
+    return deferred.promise;
 }
 
 module.exports.query = function(user) {
     var deferred = Q.defer(),
-    	url = conf.site_root + '/index.php/message/unreadcount/' + md5(user.id);
+        url = conf.site_root + '/index.php/message/unreadcount/' + md5(user.id);
 
-	request.post(
-	    url,
-	    {
-	    	form: {
-	    		'userid': user.id,
-	    		'usertype': user.type
-	    	}
-	    },
-	    function (error, response, body) {
-	    	console.info(response.body)
-	        if (error) {
-	        	console.info(response.body)
-	        	deferred.reject(error);
-	        }
-	        try {
-	        	var count = parseInt(JSON.parse(response.body).result.count) || 0;
-	        	deferred.resolve(count);
-	        } catch (e) {
-	        	deferred.resolve(0);
-	        }
-	        
-	    }
-	);
+    request.post(
+        url,
+        {
+            form: {
+                'userid': user.id,
+                'usertype': user.type
+            }
+        },
+        function (error, response, body) {
+            console.info(response.body)
+            if (error) {
+                console.info(response.body)
+                deferred.reject(error);
+            }
+            try {
+                var count = parseInt(JSON.parse(response.body).result.count) || 0;
+                deferred.resolve(count);
+            } catch (e) {
+                deferred.resolve(0);
+            }
+            
+        }
+    );
 
-	return deferred.promise;
+    return deferred.promise;
 }
