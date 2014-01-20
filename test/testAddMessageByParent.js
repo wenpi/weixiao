af6d5cea32d082e3e7e3664ae7edce9dde63cc18
panwei <hopesfish@163.com>
2014-01-21 00:56:56 +0800

    update case

--- test/testAddMessageByParent.js
+++ test/testAddMessageByParent.js
@@ -4,13 +4,13 @@ var crypto = require('crypto');
 var shasum = crypto.createHash('md5');
 
 var key = '1387432923256';
-shasum.update(key + 'rest' + 'kinderg' + '1qw23er4' + '20131231');
+shasum.update(key + 'rest' + 'kinderg' + '1qw23er4' + '20140121');
 var token = shasum.digest('hex');
 
 var mobile = "138" + (new Date()).getTime().toString().substring(5);
 
 var options = {
-    url: 'http://test.weexiao.com/api/school/d28eefe9-db3b-4db5-a469-424ac5d187d8/student/2653b8d5-f6f6-4b64-880d-3082bafa670a/message',
+    url: 'http://test.weexiao.com/api/school/a106d68b-cbfd-294a-5324-8d0a5e329e2d/student/dcec1bd4-ea36-4622-89d6-6c1aeaefd434/message',
     method: 'POST',
     headers: {
         'wexkey': key,
@@ -18,10 +18,10 @@ var options = {
     },
     form: {
         'Message[title]': "测试家长标题",
-        'Message[content]:': "测试家长内容",
+        'Message[content]:': "测试家长内容，测试推送",
         'Message[type]:': '0',
         'Message[top]:': '0',
-        'userid': "b43b7c56-5e45-11e3-83f9-00163e0426cb"
+        'userid': "fdf5dd6e-b461-42fa-a7ae-3a75718f347d"
     }
 };
 
